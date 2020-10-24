9fd95e72cac8a03bf3593b80094b01b788c0b92e
Bin Wang <bwang10@officeworks.com.au>
2020-10-24 22:17:01 +1100

    config

--- src/quiz/config.py
+++ src/quiz/config.py
@@ -1,4 +1,5 @@
 import os
 
 class Config(object):
-    DB_PATH = '/mnt/c/Users/bin/pcloud_sync/quiz'
+    # DB_PATH = '/mnt/c/Users/bin/pcloud_sync/quiz'
+    DB_PATH = '/mnt/c/pcloud_ben/personal_project/quiz'
